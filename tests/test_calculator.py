6aa645ac1e2a9a02eab6744c94b26e742ec3cfe6
Valeriy Erdukov <adizerkoo@yandex.ru>
2025-09-13 19:47:42 +0300

    added flake8 check

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,17 +3,21 @@ from calc.calculator import add, sub, mul, div
 
 
 def test_add():
-    assert add(2,5) == 7
+    assert add(2, 5) == 7
+
 
 def test_sub():
-    assert sub(7,4) == 3
+    assert sub(7, 4) == 3
+
 
 def test_mul():
-    assert mul(5,5) == 25
+    assert mul(5, 5) == 25
+
 
 def test_div():
-    assert div(16,4) == 4
+    assert div(16, 4) == 4
+
 
 def test_div_by_zero():
     with pytest.raises(ZeroDivisionError):
-        div(5,0)
+        div(5, 0)
