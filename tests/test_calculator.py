23890f24ac6a54ac99e8ac4eb3efc90585f9a4ad
Valeriy Erdukov <adizerkoo@yandex.ru>
2025-09-13 19:21:21 +0300

    Initial: simple calculator + tests

---
+++ tests/test_calculator.py
@@ -0,0 +1,19 @@
+import pytest
+from calc.calculator import add, sub, mul, div
+
+
+def test_add():
+    assert add(2,5) == 7
+
+def test_sub():
+    assert sub(7,4) == 3
+
+def test_mul():
+    assert mul(5,5) == 25
+
+def test_div():
+    assert div(16,4) == 4
+
+def test_div_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        div(5,0)
